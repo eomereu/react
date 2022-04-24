5ee265fc465b642058ea938d1f0a9303c8f86e0b
Omer <eomereu@gmail.com>
2022-04-25 00:29:45 +0200

    Using CSS Modules (skipping styled-components)

--- 02-course-goals/src/components/UI/Button/Button.js
+++ 02-course-goals/src/components/UI/Button/Button.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import './Button.css';
+import classes from './Button.module.css';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={classes.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
