695555afb02a9f217375d18903bd8217052a5db4
Omer <eomereu@gmail.com>
2022-08-28 23:51:27 +0200

    Implementing useInput Hook for Food Order App

---
+++ 05-food-order/src/hooks/use-input.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const useInput = (validate) => {
+  const [value, setValue] = useState("");
+  const [valueTouched, setValueTouched] = useState(false);
+
+  const valueIsValid = validate(value);
+  const hasError = !valueIsValid && valueTouched;
+
+  const valueChangeHandler = (event) => {
+    setValue(event.target.value);
+  };
+
+  const valueBlurHandler = () => {
+    setValueTouched(true);
+  };
+
+  const reset = () => {
+    setValue("");
+    setValueTouched(false);
+  };
+
+  return {
+    value,
+    valueIsValid,
+    hasError,
+    valueChangeHandler,
+    valueBlurHandler,
+    reset,
+  };
+};
+
+export default useInput;
